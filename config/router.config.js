9adc30a1618fd612f5acf9407abe05a866b071da
Autumnfarewell <2434185142@qq.com>
2023-07-08 20:06:37 +0800

    feat:商品分类提交3

--- config/router.config.js
+++ config/router.config.js
@@ -2,9 +2,7 @@ export default [
   {
     path: '/',
     component: '@/layouts/BasicLayout',
-    routes: [
-      { path: '/', component: './login', title: '首页' },
-    ]
+    routes: [{ path: '/', component: './login', title: '首页' }],
   },
 
   // 路由前端给后台
@@ -13,7 +11,11 @@ export default [
     component: '@/layouts/BasicLayout',
     wrappers: ['@/pages/authorized'], // 路由守卫
     routes: [
-      { path: '/user/orderManagement', component: './orderManagement', title: '订单管理' },
+      {
+        path: '/user/orderManagement',
+        component: './orderManagement',
+        title: '订单管理',
+      },
     ],
   },
-]
+];
